18780833800e746dd6d7c05ea5ded29cda24f6e2
Chinmay-biswas <94024253+Chinmay-biswas@users.noreply.github.com>
2025-06-29 02:58:51 +0530

    #r

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -8,9 +8,9 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
 const syncUserCreation =inngest.createFunction(
     {id: 'sync-user-from-clerk'},
-    { event: "clerk/user.created" },
-    async ({event})=>{
-        const{id,first_name,last_name, email_addresses, image_url} =event.data
+    { event: 'clerk/user.created' },
+    async ({ event })=>{
+        const{id,first_name,last_name, email_addresses, image_url} = event.data
         const userData ={
             _id:id,
             email: email_addresses[0].email_address,
@@ -25,7 +25,7 @@ const syncUserCreation =inngest.createFunction(
 
 const syncUserDeletion =inngest.createFunction(
     {id: 'delete-user-with-clerk'},
-    { event: "clerk/user.deleted" },
+    { event: 'clerk/user.deleted' },
     async ({event})=>{
         
         const {id}=event.data
